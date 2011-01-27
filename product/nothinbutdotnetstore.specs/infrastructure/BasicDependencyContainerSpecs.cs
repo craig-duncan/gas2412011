b6c3d763641944ac3b9a80a03c54906f77da79ea
unknown <cduncan@.invisioninc.local>
2011-01-27 10:41:31 -0500

    Pushing new changes

--- product/nothinbutdotnetstore.specs/infrastructure/BasicDependencyContainerSpecs.cs
+++ product/nothinbutdotnetstore.specs/infrastructure/BasicDependencyContainerSpecs.cs
@@ -13,10 +13,22 @@ namespace nothinbutdotnetstore.specs.infrastructure
         }
 
         [Subject(typeof(BasicDependencyContainer))]
-        public class when_observation_name : concern
+        public class when_container_is_called : concern
         {
-        
-            It first_observation = () =>        
+//            It should_tell_the_renderer_to_display_the_set_of_main_deparments = () =>
+//                renderer.received(x => x.display(the_main_departments));
+
+//                        Because b = () => { result = sut.matches(request); };
+
+            Because b = () => {
+                result = sut.a<string>();
+            };
+
+            It should_return_a_valid_impl = () =>
+                result.ShouldBeAn<string>();
+
+            static string result;        
+
                 
         }
     }
---
+++ product/nothinbutdotnetstore/infrastructure/containers/BasicDependencyContainer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace nothinbutdotnetstore.infrastructure.containers
+{
+    public class BasicDependencyContainer : DependencyContainer
+    {
+        public Dependency a<Dependency>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
